339721b4800c15dd91df4f222ee3a5c9433ef9d9
명품상자 <znlakfmzh376@gmail.com>
2026-01-13 09:31:51 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,13 @@
+import sys
+input=sys.stdin.readline
+
+S=input().strip()
+if S=="NLCS":
+    print("North London Collegiate School")
+elif S=="BHA":
+    print("Branksome Hall Asia")    
+elif S=="KIS":
+    print("Korea International School")   
+elif S=="SJA":
+    print("St. Johnsbury Academy")   
+      
