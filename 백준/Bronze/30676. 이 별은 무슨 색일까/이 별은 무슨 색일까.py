a454f8bc253ccedc102571e829298737610e3255
명품상자 <znlakfmzh376@gmail.com>
2026-01-14 11:35:38 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+a= int(input())
+if 620 <= a <=780:
+    print("Red")
+elif 590<= a:
+    print("Orange")
+elif 570<= a:
+    print("Yellow")
+elif 495<= a:
+    print("Green")
+elif 450<= a:
+    print("Blue")
+elif 425<= a:
+    print("Indigo")      
+elif 380<= a:
+    print("Violet")                          
