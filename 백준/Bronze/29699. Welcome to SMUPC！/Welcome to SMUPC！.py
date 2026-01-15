9fe787eeef1aafb679f251e033a907e65b87cef2
명품상자 <znlakfmzh376@gmail.com>
2026-01-15 13:34:41 +0900

    [Bronze V] Title: Welcome to SMUPC!, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/29699. Welcome to SMUPC！/Welcome to SMUPC！.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+# comeToSMUP
+N = int(input())
+if(N%14==0):
+        print("C")
+if(N%14==1):
+        print("W")
+if(N%14==2):
+        print("e")
+if(N%14==3):
+        print("l")
+if(N%14==4):
+        print("c")
+if(N%14==5):
+        print("o")
+if(N%14==6):
+        print("m")
+if(N%14==7):
+        print("e")
+if(N%14==8):
+        print("T")
+if(N%14==9):
+        print("o")
+if(N%14==10):
+        print("S")
+if(N%14==11):
+        print("M")
+if(N%14==12):
+        print("U")
+if(N%14==13):
+        print("P")
+                                                                                                             
+        
