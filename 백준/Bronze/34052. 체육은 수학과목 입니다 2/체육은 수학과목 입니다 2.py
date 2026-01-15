5e73b13299885557396be6d7984a3555140802e5
명품상자 <znlakfmzh376@gmail.com>
2026-01-15 14:26:29 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+if (a+b+c+d+300)>1800:
+        print("No")
+else:
+        print("Yes")        
