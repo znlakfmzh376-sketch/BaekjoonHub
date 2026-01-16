303a60e69e73cf15b6e262e750ccc11025b8a606
명품상자 <znlakfmzh376@gmail.com>
2026-01-16 20:12:43 +0900

    [Bronze V] Title: 공통교육과정, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+x= input().strip()
+if "F"== x[0]:
+                print("Foundation")    
+elif "C"==x[0]:
+        print("Claves") 
+elif "V"==x[0]:
+        print("Veritas")
+elif "E"==x[0]:
+        print("Exploration")                           
+else:
+        print()        
