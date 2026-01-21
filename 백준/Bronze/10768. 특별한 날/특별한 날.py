4f7bc789e56429bb113c853b18b57bced7ce7938
명품상자 <znlakfmzh376@gmail.com>
2026-01-21 15:26:48 +0900

    [Bronze IV] Title: 특별한 날, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+a = int(input())
+b = int(input())
+
+if a == 2 and b == 18:
+    print("Special")
+elif a < 2 or (a == 2 and b < 18):
+    print("Before")
+else:
+    print("After")
