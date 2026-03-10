b64c109ecdc3afd46547df919982aa7676398031
명품상자 <znlakfmzh376@gmail.com>
2026-03-10 13:20:38 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 34668 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,14 +1,8 @@
 import sys
+import math
 input = sys.stdin.readline
 N,K=map(int, input().split())
-n_factorial=1
-k_factorial=1   
-t_factorial=1
-for i in range(1,N+1,1):
-    n_factorial*=i
-for i in range(1,K+1,1):
-    k_factorial*=i
-for i in range(1,N-K+1,1):
-    t_factorial*=i
-result=n_factorial//(k_factorial*t_factorial)
+
+result=math.factorial(N)//(math.factorial(K)*math.factorial(N-K))
+
 print(result)
