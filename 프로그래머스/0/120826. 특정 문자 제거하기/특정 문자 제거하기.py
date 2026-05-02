033f1412e8c8f97facea596346701cd3de5f85bb
명품상자 <znlakfmzh376@gmail.com>
2026-05-02 19:07:43 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, letter):
+    answer = ''
+    for char in my_string:
+        if char==letter:
+            pass
+        else:
+            answer += char
+    return answer
