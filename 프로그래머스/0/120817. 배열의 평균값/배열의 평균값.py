7366344928abe23319027518cfbd4aeba4212b8f
명품상자 <znlakfmzh376@gmail.com>
2026-05-02 18:43:34 +0900

    [level 0] Title: 배열의 평균값, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    answer = sum(numbers)/len(numbers)
+    return answer
+
+print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(solution([89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]))
