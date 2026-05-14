f36e25e26497d165929d3b647293a1d6da38f826
명품상자 <znlakfmzh376@gmail.com>
2026-05-14 13:48:27 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.00 ms, Memory: 8.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -0,0 +1,26 @@
+def solution(age):
+    answer=''
+    age=str(age)
+    for i in age:
+        if i=='0':
+            answer +='a'
+        elif i=='1':
+            answer +='b'
+        elif i=='2':
+            answer +='c'
+        elif i=='3':
+            answer +='d'
+        elif i=='4':
+            answer +='e'
+        elif i=='5':
+            answer +='f'
+        elif i=='6':
+            answer +='g'
+        elif i=='7':
+            answer +='h'
+        elif i=='8':
+            answer +='i'
+        elif i=='9':
+            answer +='j'    
+            
+    return "".join(answer)
