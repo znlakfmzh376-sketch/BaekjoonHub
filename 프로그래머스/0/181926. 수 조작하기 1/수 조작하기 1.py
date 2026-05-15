65c589b3a4a8ac7a6ec78dfe34926296a92e6982
명품상자 <znlakfmzh376@gmail.com>
2026-05-15 14:16:18 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.74 ms, Memory: 9.44 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,15 @@
+def solution(n, control):
+    answer = 0
+    for i in control:
+        if i =='w':
+            n +=1
+        elif i=="s":
+            n-=1
+        elif i=='d':
+            n+=10
+        elif i=='a':
+            n-=10
+        else:
+            pass
+    answer=n
+    return answer
