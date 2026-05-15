294fe9ce47db89496c005bf5097ccf92193c5963
명품상자 <znlakfmzh376@gmail.com>
2026-05-15 15:27:53 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = 0
+    odd=''
+    even=''
+    for i in num_list:
+        if i%2==0:
+            odd += str(i)
+        else:
+            even += str(i)
+    answer=int(odd) + int(even)    
+    return answer
