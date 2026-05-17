0c44ff4be06d66d3cb7adf48056e852980251670
명품상자 <znlakfmzh376@gmail.com>
2026-05-17 22:03:59 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = 0
+    arr=list(s.split(" "))
+    for i in range(len(arr)):
+        if arr[i] =='Z':
+            answer -= int(arr[i-1])
+        else:
+            answer += int(arr[i])
+    return answer
