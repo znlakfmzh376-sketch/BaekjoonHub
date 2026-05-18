e342258d3c99dae8a5e5bad6146107dc17a02bf4
명품상자 <znlakfmzh376@gmail.com>
2026-05-18 23:21:01 +0900

    [level 0] Title: 배열 만들기 5, Time: 2.08 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -0,0 +1,8 @@
+def solution(intStrs, k, s, l):
+    answer = []
+    for i in intStrs:
+        if k<int(i[s:s+l]):
+            answer.append(int(i[s:s+l]))
+        else:
+            pass
+    return answer
