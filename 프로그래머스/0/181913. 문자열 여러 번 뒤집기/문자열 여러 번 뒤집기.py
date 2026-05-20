6cc22cb5ae6868d969307a9c06b6898bd2c3dc97
명품상자 <znlakfmzh376@gmail.com>
2026-05-20 14:36:08 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 0.88 ms, Memory: 9.42 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -0,0 +1,6 @@
+def solution(my_string, queries):
+    answer = ''
+    for i,j in queries:
+        my_string = my_string[0:i]+my_string[i:j+1][::-1]+my_string[j+1:]
+        
+    return my_string
