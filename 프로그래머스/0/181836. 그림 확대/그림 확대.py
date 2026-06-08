2a097f4dfaf18cd67bc22004d59683fe277e2ee8
명품상자 <znlakfmzh376@gmail.com>
2026-06-08 21:41:22 +0900

    [level 0] Title: 그림 확대, Time: 0.04 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -0,0 +1,9 @@
+def solution(picture, k):
+    answer = []
+    for i in range(len(picture)):
+        temp =""
+        for j in range(len(picture[i])):
+            temp+=(picture[i][j]*k)
+        for l in range(k):
+            answer.append(temp)
+    return answer
