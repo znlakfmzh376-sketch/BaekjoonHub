494da1847f100858473a139d04be1d38a9e99c25
명품상자 <znlakfmzh376@gmail.com>
2026-06-22 10:04:11 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = 0
+    nums =[1,2,3,4,5,6,7,8,9,0]
+    for i in nums:
+        if i in numbers:
+            answer += 0
+        else:
+            answer += i
+                
+    return answer
